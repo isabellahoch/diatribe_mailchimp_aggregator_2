1e9bc24fe8be4427591609065ce0631796d09281
Patrick Frenett <patrick@frenett.net>
2018-07-25 16:43:05 -0700

    Added all previous HTML emails

--- flask/MailChimp.py
+++ flask/MailChimp.py
@@ -69,6 +69,18 @@ class MailChimpCampaign:
 			return False
 		return response.json()
 
+	def get_clicked_links(self):
+		request_auth = (self.user, self.api_key)
+		request_url = "https://" + self.region + ".api.mailchimp.com/3.0/reports/" + self.id + "/click-details?count=10000"
+		response = requests.get(request_url, auth=request_auth)
+		if response.status_code != 200:
+			return False
+		
+		urls_clicked = response.json()["urls_clicked"]
+		print(len(urls_clicked))
+		print(urls_clicked)
+
+
 	def get_email_content_links_by_type(self):
 		content = self.get_content()
 		type_1_img_src = "http://diatribe.org/sites/default/files/styles/diabetes_icon_articles/public/type-1.png"
@@ -80,52 +92,49 @@ class MailChimpCampaign:
 		all_links = {"t1":list(), "t2":list(), "t1t2":list()}
 
 		# Get all links next to the desired image references
-		if "variate_contents" not in content:
+		if "variate_contents" not in content and "html" in content:
 			html = content["html"]
 			f = open("../email_html_examples/"+self.id+".html", "w")
 			f.write(html)
 			f.close()
 			tabs = html.split("<table")
 			for table_div in tabs:
-				if type_1_img_src in table_div:
+				if len(table_div.split("href=\"")) > 1:
 					link = table_div.split("href=\"")[1].split("\"")[0]
+				else:
+					continue
+				if type_1_img_src in table_div:
 					if link not in all_links["t1"]:
 						all_links["t1"].append(link)
 				if type_2_img_src in table_div:
-					link = table_div.split("href=\"")[1].split("\"")[0]
 					if link not in all_links["t2"]:
 						all_links["t2"].append(link)
 				if type_1_2_img_src in table_div:
-					link = table_div.split("href=\"")[1].split("\"")[0]
 					if link not in all_links["t1t2"]:
 						all_links["t1t2"].append(link)
-		else:
+		if "variate_contents" in content:
 			for variate in content["variate_contents"]:
+				if "html" not in variate:
+					continue
 				html = variate["html"]
 				f = open("../email_html_examples/"+self.id+".html", "w")
 				f.write(html)
 				f.close()
 				tabs = html.split("<table")
 				for table_div in tabs:
-					if type_1_img_src in table_div:
+					if len(table_div.split("href=\"")) > 1:
 						link = table_div.split("href=\"")[1].split("\"")[0]
+					else:
+						continue
+					if type_1_img_src in table_div:
 						if link not in all_links["t1"]:
 							all_links["t1"].append(link)
 					if type_2_img_src in table_div:
-						link = table_div.split("href=\"")[1].split("\"")[0]
 						if link not in all_links["t2"]:
 							all_links["t2"].append(link)
 					if type_1_2_img_src in table_div:
-						link = table_div.split("href=\"")[1].split("\"")[0]
 						if link not in all_links["t1t2"]:
 							all_links["t1t2"].append(link)
-
-
-		print("\n\n\n\n")
-		print(content)
-		print("\n\n\n\n")
-		exit()
-
 
 		return all_links
 
@@ -133,13 +142,22 @@ class MailChimpCampaign:
 
 
 test = MailChimp(mailchimp_api_key, mailchimp_region, mailchimp_user)
-camp_ids = test.get_campaigns(get_all=False, return_ids=True)
+camp_ids = test.get_campaigns(get_all=True, return_ids=True)
+
+"""
+camp = MailChimpCampaign(test, camp_ids[0])
+camp.get_clicked_links()
+print(camp.get_email_content_links_by_type())
+exit()
+"""
 
 for camp_id in camp_ids:
-
-	camp = MailChimpCampaign(test, camp_id)
 	print(camp_id)
-	print(camp.get_email_content_links_by_type())
+	camp = MailChimpCampaign(test, camp_id)
+	camp.get_email_content_links_by_type()
+
+print(len(camp_ids))
+
 
 
 
